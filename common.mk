effc3819cca21c393f58770a5e20198a7220c15a
Hasan Okarci <frazerecho@users.noreply.github.com>
2022-08-12 15:32:57 +0500

    Activate Quick Tap support

--- common.mk
+++ common.mk
@@ -392,6 +392,9 @@ PRODUCT_PACKAGES += \
     libjson \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor
+    
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true
 
 # RIL
 PRODUCT_PACKAGES += \
